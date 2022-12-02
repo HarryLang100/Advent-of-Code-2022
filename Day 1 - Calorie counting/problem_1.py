d9873c165f2f5c17774286d30a41f48b77211653
HarryLang100 <harrylang100@gmail.com>
2022-12-02 12:03:11 +0000

    Improve the way the text file is read

--- Day 1 - Calorie counting/problem_1.py
+++ Day 1 - Calorie counting/problem_1.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec  1 10:48:38 2022
+Created on Fri Dec  2 11:51:50 2022
 
 @author: Harry Lang
 """
 
-import pandas as pd
-
-file = pd.read_table("calories_input.txt", header=None)
-
 current_sum = 0
 max_sum = 0
-for current_row in range(file.shape[0]):
-    if file.iloc[current_row][0] == -1:
-        current_sum = 0
+
+with open("original_input.txt") as input_file:
+    lines = input_file.readlines()
+    
+for current_row in lines:
+    if current_row.strip() == "": # If the line is empty.
+        current_sum = 0 # Reset the counter.
     else:
-        current_sum += file.iloc[current_row][0]
+        current_sum += int(current_row.strip()) # Add the current number to
+        # the count. 
     if current_sum > max_sum:
         max_sum = current_sum
 
 print(f"Result: {max_sum}")
-
--- Day 1 - Calorie counting/problem_2.py
+++ Day 1 - Calorie counting/problem_2.py
@@ -5,20 +5,18 @@ Created on Thu Dec  1 10:48:38 2022
 @author: Harry Lang
 """
 
-import pandas as pd
-
-file = pd.read_table("calories_input.txt", header=None)
+with open("original_input.txt") as input_file:
+    lines = input_file.readlines()
 
 current_sum = 0
 results_list = []
-for current_row in range(file.shape[0]):
-    if file.iloc[current_row][0] == -1:
+for current_line in lines:
+    if current_line.strip() == "":
         results_list.append(current_sum)
         current_sum = 0
     else:
-        current_sum += file.iloc[current_row][0]
+        current_sum += int(current_line.strip())
 
 results_list.sort(reverse=True)
-print(results_list)
 top_three_sum = results_list[0] + results_list[1] + results_list[2]
 print(f"Sum: {top_three_sum}")
